1865b4e045aa8c2a2a41dc969038921479d0c1b7
zzthomaszzz <60630447+zzthomaszzz@users.noreply.github.com>
2020-02-20 09:13:19 +1300

    scale factor but no valid check

---
+++ 03_find_scale_fator.py
@@ -0,0 +1,9 @@
+
+
+
+# main route here
+serving_size = float(input('what is the serving size '))
+desired_size = float(input('how many serving are needed '))
+
+scale_factor = desired_size / serving_size
+print('the scale factor is',scale_factor)

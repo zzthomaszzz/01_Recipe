3eb9ad29f49a588940143cdaa41c58accce50fb5
zzthomaszzz <60630447+zzthomaszzz@users.noreply.github.com>
2020-02-20 09:28:38 +1300

    working scal factor

--- 03_find_scale_fator.py
+++ 03_find_scale_fator.py
@@ -1,9 +1,25 @@
+# Funtions go here
 
+
+# number checking function
+def num_check(question):
+    error = 'please enter a number that is more than zero'
+    valid = False
+    while not valid:
+        try:
+            response = float(input(question))
+            if response <= 0:
+                print(error)
+            else:
+                return response
+
+        except ValueError:
+            print('please enter a number ')
 
 
 # main route here
-serving_size = float(input('what is the serving size '))
-desired_size = float(input('how many serving are needed '))
+serving_size = num_check('what is the serving size for the recipe ')
+desired_size = num_check('how many serving are needed ')
 
 scale_factor = desired_size / serving_size
 print('the scale factor is',scale_factor)

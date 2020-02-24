9c64dbd097f65cd554f6cbaa939eaa14794a5213
zzthomaszzz <60630447+zzthomaszzz@users.noreply.github.com>
2020-02-25 12:05:45 +1300

    working scale factor

--- 03_find_scale_fator.py
+++ 03_find_scale_fator.py
@@ -17,23 +17,25 @@ def num_check(question):
 
 
 # main route here
-sf_ok ='no'
-while sf_ok == 'no':
+dodgy_sf ='yes'
+while dodgy_sf == 'yes':
     serving_size = num_check('what is the serving size for the recipe ')
     desired_size = num_check('how many serving are needed ')
     scale_factor = desired_size / serving_size
 
     if scale_factor < 0.25:
-        sf_ok = input("Warning: this scale factor is very small "
+        dodgy_sf = input("Warning: this scale factor is very small "
                       "and you might struggle to accurately weigh"
                       "the ingredients. \n"
-                      "do you want to keep going (type 'no' to change"
-                      "your desired serving size )")
+                      "do you want to change your serving size? (type 'yes' to change"
+                      "your desired serving size )").lower()
     elif scale_factor > 4:
-        sf_ok = input("Warning: This scale factor is quite large - you might"
+        dodgy_sf = input("Warning: This scale factor is quite large - you might"
                       "have issues with mixing bowl space / oven space. \n"
-                      "Do you want to keep goin (type 'no' to change"
-                      "your desired serving size) ")
+                      "Do you want to change your serving size? (type 'yes' to change"
+                      "your desired serving size) ").lower()
+    else:
+        dodgy_sf = 'no'
 
 
 

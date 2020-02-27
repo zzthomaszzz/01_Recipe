3297183e3694577802c3da905513f11fbc31f677
jgottschalkmhs <49209767+jgottschalkmhs@users.noreply.github.com>
2020-02-28 07:32:38 +1300

    Update 05_scale_ingredients.py
    This code should now work.  I shifted the code that asks for the ingredient name so that it is AFTER the if block in lines 62-72.  This means that if the users have more than 2 ingredients and they enter the exit code, they are not asked to put in an ingredient name (the 'break' command breaks out of the loop).  If the user tries to exit with too few items in the list, the program will give an error and the code now uses the 'continue' command to restart the loop (line 69-70)

--- 05_scale_ingredients.py
+++ 05_scale_ingredients.py
@@ -59,24 +59,25 @@ while stop != 'xxx':
 
     amount = num_check("What is the amount for the ingredient? ")
 
-    # Ask user for ingredient (via no blank funcion)
-    get_ingredient = not_blank("Please type in an ingredient name: ",
-                               "this can't be blank",
-                               "yes")
-    # If exit code is typed and there are more than 2 ingredients...
-
+    # If exit code is typed and there are more than 2 ingredients
+    # break out of loop
     if amount == 'xxx' and len(ingredients) > 1:
         break
+    
+    # If exit code is typed and there are less than 2 ingredients, 
+    # show error and go back to start of loop
     elif amount =='xxx' and len(ingredients) < 2:
         print('You need at least two ingredients in the list. ',
               'please add more ingredients')
-    # if exit code is not entered, add ingredient to list
-    else:
-        ''' get_ingredient = not_blank("Please type in an ingredient name: ",
+        continue
+
+    # Ask user for ingredient (via no blank funcion)
+    get_ingredient = not_blank("Please type in an ingredient name: ",
                                    "this can't be blank",
-                                   "yes")b'''
-        amount = float(amount) * scale_factor
-        ingredients.append('{} units {}'.format(amount,get_ingredient))
+                                   "yes")
+    
+    amount = float(amount) * scale_factor
+    ingredients.append('{} units {}'.format(amount,get_ingredient))
 
 print('Your ingredient list: ',ingredients)
 

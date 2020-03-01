658d94559e38ea615d85255bae840f99a4b4c9b4
zzthomaszzz <60630447+zzthomaszzz@users.noreply.github.com>
2020-03-02 12:56:14 +1300

    broken scale factor maybe

--- 05_scale_ingredients.py
+++ 05_scale_ingredients.py
@@ -58,13 +58,25 @@ stop = ''
 while stop != 'xxx':
 
     amount = num_check("What is the amount for the ingredient? ")
+    '''if amount == 'xxx' and len(ingredients) > 1:
+        break
+    elif amount =='xxx' and len(ingredients) < 2:
+        print('You need at least two ingredients in the list. ',
+              'please add more ingredients')
+    # if exit code is not entered, add ingredient to list
+    else:
+        ''' get_ingredient = not_blank("Please type in an ingredient name: ",
+                                   "this can't be blank",
+                                   "yes")b'''
+        amount = float(amount) * scale_factor
+        ingredients.append('{} units {}'.format(amount,get_ingredient))'''
+
 
     # Ask user for ingredient (via no blank funcion)
     get_ingredient = not_blank("Please type in an ingredient name: ",
                                "this can't be blank",
                                "yes")
     # If exit code is typed and there are more than 2 ingredients...
-
     if amount == 'xxx' and len(ingredients) > 1:
         break
     elif amount =='xxx' and len(ingredients) < 2:

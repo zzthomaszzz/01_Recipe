0b16c89951cc18a82dd0fd11dcf147a3a835ac7c
zzthomaszzz <60630447+zzthomaszzz@users.noreply.github.com>
2020-03-06 14:45:06 +1300

    working unit conversion

---
+++ 06_convertin.py
@@ -0,0 +1,28 @@
+# ask user for amount
+# ask user for Unit
+# check that unit is in dictionary
+
+# if unit in dictionary, convert to mL
+
+# if no unit given / unit is unknown, leave as is.
+
+unit_central = {
+    "tsp": 5,
+    "tbs": 15,
+    "cup": 237,
+    "ounce:": 30,
+    "pint": 473,
+    "quart": 946,
+    "pound": 454
+}
+amount = eval(input("How much? "))
+amount = float(amount)
+unit = input(" Unit? ")
+
+if unit in unit_central:
+    mult_by = unit_central.get(unit)
+    amount = amount * mult_by
+    print("Amount in mL {}".format(amount))
+else:
+    print("{} is unchanged".format(amount))
+
